90675949c0d0e2bf340d95a6d0a86d0cc58b7cf4
Sergey Zaligin <webdevjsphp@gmail.com>
2018-09-23 12:32:23 +0300

    fix

--- engine/core/libs/Pagination.php
+++ engine/core/libs/Pagination.php
@@ -1,33 +1,78 @@
-<?php
+<?php declare(strict_types=1);
 
 namespace engine\libs;
 
 /**
- * Description of Pagination
+ * Class Pagination
  *
  * @author sergey
  */
 class Pagination 
 {
+    /**
+     * Current page
+     * 
+     * @var int 
+     */
     public $currentPage;
+    /**
+     * Count items on one page
+     * 
+     * @var int
+     */
     public $perpage;
+    /**
+     * All items
+     * 
+     * @var int 
+     */
     public $total;
+    /**
+     * Count all pages
+     * 
+     * @var int
+     */
     public $countPages;
+    /**
+     * Uri
+     * 
+     * @var string
+     */
     public $uri;
-
-    public function __construct($page, $perpage, $total){
+    
+    /**
+     * Constructor pagination
+     * 
+     * @param int $page
+     * @param int $perpage
+     * @param int $total
+     */
+    public function __construct(int $page, int $perpage, int $total)
+    {
         $this->perpage = $perpage;
         $this->total = $total;
         $this->countPages = $this->getCountPages();
         $this->currentPage = $this->getCurrentPage($page);
         $this->uri = $this->getParams();
     }
-
-    public function __toString(){
+    
+    /**
+     * Return html
+     * 
+     * @return string
+     */
+    public function __toString(): string
+    {
         return $this->getHtml();
     }
 
-    public function getHtml(){
+    /**
+     * build html view pagination
+     * 
+     * @return string
+     */
+    public function getHtml(): string
+    {
         $back = null; // ссылка НАЗАД
         $forward = null; // ссылка ВПЕРЕД
         $startpage = null; // ссылка В НАЧАЛО
@@ -66,29 +111,55 @@ class Pagination
 
         return '<ul class="pagination">' . $startpage.$back.$page2left.$page1left.'<li class="active"><a>'.$this->currentPage.'</a></li>'.$page1right.$page2right.$forward.$endpage . '</ul>';
     }
-
-    public function getCountPages(){
-        return ceil($this->total / $this->perpage) ?: 1;
+    
+    /**
+     * Get count page
+     * 
+     * @return int
+     */
+    public function getCountPages(): int
+    {
+        return (int)ceil($this->total / $this->perpage) ?: 1;
     }
-
-    public function getCurrentPage($page){
+    
+    /**
+     * Get current page
+     * 
+     * @param int $page
+     * @return int
+     */
+    public function getCurrentPage(int $page): int
+    {
         if(!$page || $page < 1) $page = 1;
         if($page > $this->countPages) $page = $this->countPages;
         return $page;
     }
-
-    public function getStart(){
+    
+    /**
+     * Get start position for LIMIT 0, 1
+     * 
+     * @return int
+     */
+    public function getStart(): int
+    {
         return ($this->currentPage - 1) * $this->perpage;
     }
-
-    public function getParams(){
+    
+    /**
+     * Get query string params
+     * 
+     * @return string
+     */
+    public function getParams(): string
+    {
         $url = $_SERVER['REQUEST_URI'];
         $url = explode('?', $url);
         $uri = $url[0] . '?';
-        if(isset($url[1]) && $url[1] != ''){
+        
+        if (isset($url[1]) && $url[1] != '') {
             $params = explode('&', $url[1]);
-            foreach($params as $param){
-                if(!preg_match("#page=#", $param)) $uri .= "{$param}&amp;";
+            foreach ($params as $param) {
+                if (!preg_match("#page=#", $param)) $uri .= "{$param}&amp;";
             }
         }
         return $uri;
